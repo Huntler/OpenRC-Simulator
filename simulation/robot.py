34e5591146890fb58fb8c6caab0eac4925b2afa5
luisevogt <luise.vogt@student.maastrichtuniversity.nl>
2022-02-10 14:59:45 +0100

    sensor model, collision detection

--- simulation/robot.py
+++ simulation/robot.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 
+from skspatial.objects import Line, Circle
+
 
 class Robot:
     def __init__(self, midpoint, l, theta):
@@ -16,6 +18,14 @@ class Robot:
 
         self.l_wheel_pos = (midpoint[0] - l / 2, midpoint[1])
         self.r_wheel_pos = (midpoint[0] + l / 2, midpoint[1])
+
+        pi = math.pi
+        self._sensor_lenght = 100
+        self.sensor_lines = [
+            np.array([self.midpoint[0] + math.cos(2 * pi / 12 * x) * self._sensor_lenght, self.midpoint[1] +
+                      math.sin(2 * pi / 12 * x) * self._sensor_lenght])
+            for x in range(0, 12 + 1)]
+        self.distances = []
 
     def _calc_velocities(self, omega, r):
         """
@@ -71,5 +81,63 @@ class Robot:
 
         rotation_matrix = np.array([math.cos(omega * delta * t), - math.sin(omega * delta * t)])
 
+        # rotate robot
         self.midpoint = rotation_matrix * (self.midpoint - icc) + icc
         self._theta = self._theta + omega * delta * t
+
+        # rotate sensor lines
+        pi = math.pi
+        self.sensor_lines = [
+            np.array([self.midpoint[0] + math.cos(2 * pi / 12 * x) * self._sensor_lenght, self.midpoint[1] +
+                      math.sin(2 * pi / 12 * x) * self._sensor_lenght])
+            for x in range(0, 12 + 1)]
+
+    def calc_distance(self, line):
+        """
+        calculates distance to a line for every sensor line
+        :param line: the line to which the distance should be calculated
+        """
+        distances = []
+        line = Line(point=line[0], direction=line[1])
+
+        for s_line in self.sensor_lines:
+            # transform arrays to scikit Lines
+            sensor_line = Line(point=self.midpoint, direction=s_line)
+            point_intersection = line.intersect_line(sensor_line)
+            distance = point_intersection.distance_point(self.midpoint)
+
+            # append distance if it is less than 200
+            if distance > 200:
+                distances.append(200)
+            else:
+                distances.append(distance)
+
+        self.distances = distances
+
+    def collision(self, line):
+        """
+        calculates collision points of robot with a line
+        :param line: the line the robot collided with
+        :return: the collision points
+        """
+        robot_body = Circle(self.midpoint, self._l / 2)
+        line = Line(line[0], line[1])
+        try:
+            point_a, point_b = robot_body.intersect_line(line)
+        except ValueError:
+            return None
+
+        return point_a, point_b
+
+
+if __name__ == '__main__':
+    robot = Robot(np.array([-75, 50]), 10, 0)
+    # print(robot.collision([np.array([0, 0]), np.array([7, 7])]))
+
+    # import matplotlib.pyplot as plt
+    #
+    # fig, ax = plt.subplots()
+    #
+    # ax.scatter([x[0] for x in robot.sensor_lines], [x[1] for x in robot.sensor_lines], vmin=0, vmax=100)
+    # ax.plot(np.linspace(-100, 100, 10), np.linspace(-100, 100, 10))
+    # plt.show()
