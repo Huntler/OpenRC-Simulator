d7465be313e555f68a0e95f01c256da99d066989
Jonas Leuchtenberger <jonleu@freenet.de>
2022-02-21 13:35:50 +0100

    Classes for algorithm + simple NN

---
+++ algorithm/robot_genome.py
@@ -0,0 +1,85 @@
+from random import randint, random, randrange
+from typing import List, Tuple
+
+import numpy as np
+
+
+class RobotGenome:
+    def __init__(self, sensor_num: int = 12, motor_num: int = 2, hidden_layer_size: int = 4) -> None:
+        # randomly initialize the input layer weights
+        self._input_layer_weights = np.array([[random() for _ in range(hidden_layer_size)] for _ in range(sensor_num)])
+        
+        # randomly initilize the output layer weights
+        self._output_layer_weights = np.array([[random() for _ in range(motor_num)] for _ in range(hidden_layer_size)])
+
+    def fitness(self) -> int:
+        pass
+
+    def drive(self, sensors: np.ndarray) -> Tuple[float]:
+        # forward passthrough the sensors into our NN to get the motors acceleration
+        x = self._input_layer_weights * sensors
+        x = self._output_layer_weights * x
+
+        # 1 means accelerate, -1 breake and 0 nothing
+        return np.sign(x)
+    
+    def mutate(self, num: int = 1, probability: float = 0.5) -> None:
+        for _ in range(num):
+            # chose a layer randomly
+            layer = random()
+            if layer > 0.5:
+                # select a random weight
+                index = randrange(len(self._input_layer_weights))
+
+                # change it with a chance of the given probability
+                value = self._input_layer_weights[index]
+                if random() > probability:
+                    value = random()
+                self._input_layer_weights[index] = value
+            else:
+                # select a random weight
+                index = randrange(len(self._output_layer_weights))
+
+                # change it with a chance of the given probability
+                value = self._output_layer_weights[index]
+                if random() > probability:
+                    value = random()
+                self._output_layer_weights[index] = value
+
+    
+    @staticmethod
+    def crossover(a: "RobotGenome", b: "RobotGenome") -> Tuple["RobotGenome", "RobotGenome"]:
+        # get the genomes parameter
+        sensor_num = len(a._input_layer_weights)
+        hidden_layer_size = len(a._output_layer_weights)
+        motor_num = a._output_layer_weights.shape[0] * a._output_layer_weights.shape[1] / hidden_layer_size
+
+        # and create new ones
+        new_a = RobotGenome(sensor_num, motor_num, hidden_layer_size)
+        new_b = RobotGenome(sensor_num, motor_num, hidden_layer_size)
+
+        # chose random weights from the provided genomes a and b
+        p_1 = randint(1, sensor_num - 1)
+        p_2 = randint(1, hidden_layer_size - 1)
+        p_3 = randint(1, motor_num - 1)
+
+        # perform crossover for the first layer
+        a_crossover = a._input_layer_weights[0:p_1, 0:p_2] + b._input_layer_weights[p_1:, p_2:]
+        new_a._input_layer_weights = a_crossover
+
+        b_crossover = b._input_layer_weights[0:p_1, 0:p_2] + a._input_layer_weights[p_1:, p_2:]
+        new_b._input_layer_weights = b_crossover
+
+        # perform crossover for the second layer
+        a_crossover = a._output_layer_weights[0:p_2, 0:p_3] + b._output_layer_weights[p_2:, p_3:]
+        new_a._output_layer_weights = a_crossover
+
+        b_crossover = b._output_layer_weights[0:p_2, 0:p_3] + a._output_layer_weights[p_2:, p_3:]
+        new_b._output_layer_weights = b_crossover
+        
+        return new_a, new_b
+
+
+
+if __name__ == "__main__":
+    print(np.sign([-123, 0.435]))
---
+++ algorithm/robot_population.py
@@ -0,0 +1,61 @@
+from random import choices
+from typing import Tuple
+from algorithm.robot_genome import RobotGenome
+
+
+class RobotPopulation:
+    def __init__(self, robot_num) -> None:
+        self._population = [RobotGenome() for _ in range(robot_num)]
+    
+
+    def _select_pair(self) -> Tuple[RobotGenome, RobotGenome]:
+        # select two genomes based on their fitness value
+        return choices(
+            population=self._population,
+            weights=[genome.fitness() for genome in self._population],
+            k=2
+        )
+
+    def run_evolution(self, generation_limit: int = 100, fitness_limit: int = 100) -> Tuple["RobotPopulation", int]:
+        for i in range(generation_limit):
+            # sort the population based on the genome's fitness
+            self._population = sorted(
+                self._population,
+                key=lambda genome: genome.fitness(),
+                reverse=True
+            )
+
+            # if this generation includes a genome with the maximum fitness specified
+            # then break -> best population found
+            if self._population[0].fitness() >= fitness_limit:
+                break
+
+            # create a new generation based on the best two genomes
+            next_generation = self._population[:2]
+            for j in range(len(self._population) // 2 - 1):
+                # also select parents from the old population
+                parents = self._select_pair()
+
+                # and create childrens using the crossover function
+                offspring_a, offspring_b = RobotGenome.crossover(parents[0], parents[1])
+
+                # do not forget to mutate those children to inject the evolutionary approach
+                offspring_a.mutate()
+                offspring_b.mutate()
+
+                # then add those children to the new generation
+                next_generation += [offspring_a, offspring_b]
+
+            # new generation build up -> repeat process
+            self._population = next_generation
+
+        # the best population was found (or the limit has been reached)
+        # sort the population based on the genome's fitness and return it
+        # together with the amount of generations simulated
+        population = sorted(
+            self._population,
+            key=lambda genome: genome.fitness(),
+            reverse=True
+        )
+        
+        return population, i
